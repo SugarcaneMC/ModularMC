28edbc16383ba816ca0aa1cf6448f3d48094bf85
CodersInABarrel <57611311+CodersInABarrel@users.noreply.github.com>
2021-09-25 21:43:38 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::net;
-
+use std::thread;
 fn main() {
-    let listener = net::TcpListener::bind("127.0.0.1:8080");
+    let boss_thread = thread::spawn(|| {
+        let listener = net::TcpListener::bind("127.0.0.1:8080").unwrap();
+
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+
+            let worker = thread::spawn(move || {
+                let stream = stream; // move stream into this func
+            }).join().unwrap();
+        }
+
+    }).join().unwrap();
 }
