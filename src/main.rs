5d789e5d6bec602ef0e82beca1f31d59215ff532
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2021-10-10 18:07:08 -0400

    _Snake_case_go_brrr
    Fixing all your snake case issues since 5 minutes ago!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::net;
 use std::thread;
 fn main() {
-    let boss_thread = thread::spawn(|| {
+    let _boss_thread = thread::spawn(|| {
         let listener = net::TcpListener::bind("127.0.0.1:8080").unwrap();
 
         for stream in listener.incoming() {
-            let stream = stream.unwrap();
+            let _stream = stream.unwrap();
 
-            let worker = thread::spawn(move || {
-                let stream = stream; // move stream into this func
+            let _worker = thread::spawn(move || {
+                let _stream = stream; // move stream into this func
             }).join().unwrap();
         }
 
